3fe1ae0acba3426dbd825eef9c939089c2dca892
CarloHD <carloborrometi@gmail.com>
2023-11-09 15:13:23 +0100

    ottimizzazione css

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core'
 import { Post } from '../models/post.model'
 import { HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment'
+import { of, tap } from 'rxjs'
 
 const authEndpoint = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.apiKey}`
 const databaseDateEndpoint =
@@ -15,26 +16,58 @@ const databasePostsEndpoint =
 export class FirebaseService {
   constructor (private http: HttpClient) {}
 
-  saveOnDatabase (date: number, posts: Post[]) {
-    this.http
-      .post<{ idToken: string }>(authEndpoint, {
+  authToken: string | null = null
+  autTokenExpirationTimer: any
+
+  getAuth () {
+    this.authToken = localStorage.getItem('authToken')
+    const authExpiration = localStorage.getItem('authExpiration')
+
+    if (authExpiration && +authExpiration < Date.now()) {
+      this.authToken = null
+    }
+
+    if (this.authToken !== null && authExpiration !== null) {
+      this.autTokenExpirationTimer = setTimeout(() => {
+        this.authToken = null
+      }, +authExpiration - Date.now())
+      return of({ idToken: this.authToken })
+    }
+
+    return this.http
+      .post<{ idToken: string; expireIn: string }>(authEndpoint, {
         returnSecureToken: true
       })
-      .subscribe(res => {
-        this.http
-          .put(
-            `${databaseDateEndpoint}?auth=${res.idToken}`,
-            JSON.stringify(date)
+      .pipe(
+        tap(res => {
+          localStorage.setItem('authToken', res.idToken)
+          localStorage.setItem(
+            'authExpiration',
+            (Date.now() + +res.expireIn * 1000).toString()
           )
-          .subscribe()
+          this.autTokenExpirationTimer = setTimeout(() => {
+            this.authToken = null
+          }, +res.expireIn * 1000)
+        })
+      )
+  }
 
-        this.http
-          .put(
-            `${databasePostsEndpoint}?auth=${res.idToken}`,
-            JSON.stringify(posts)
-          )
-          .subscribe()
-      })
+  saveOnDatabase (date: number, posts: Post[]) {
+    this.getAuth().subscribe(res => {
+      this.http
+        .put(
+          `${databaseDateEndpoint}?auth=${res.idToken}`,
+          JSON.stringify(date)
+        )
+        .subscribe()
+
+      this.http
+        .put(
+          `${databasePostsEndpoint}?auth=${res.idToken}`,
+          JSON.stringify(posts)
+        )
+        .subscribe()
+    })
   }
 
   getLastUpdate () {
